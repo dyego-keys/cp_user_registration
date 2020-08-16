04dc860f1a85358d5adc03e59daa90760827e361
Diego Chavez <dyego.chavez@gmail.com>
2020-08-17 05:28:42 +1200

    Ignoring status check from security

--- src/main/java/com/countiespower/api/user/register/config/WebSecurityConfig.java
+++ src/main/java/com/countiespower/api/user/register/config/WebSecurityConfig.java
@@ -45,7 +45,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/users/login").permitAll().
+                .authorizeRequests().antMatchers("/users/login").permitAll().and().
+                authorizeRequests().antMatchers("/users/status/check").permitAll().
         anyRequest().authenticated().and().
         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
