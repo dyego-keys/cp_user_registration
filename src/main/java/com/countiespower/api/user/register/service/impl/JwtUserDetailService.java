ea7d70a5bb27351c82091b219522b02ae983a83d
Diego Chavez <dyego.chavez@gmail.com>
2020-08-16 23:59:03 +1200

    Changes in security/token login

--- src/main/java/com/countiespower/api/user/register/service/impl/JwtUserDetailService.java
+++ src/main/java/com/countiespower/api/user/register/service/impl/JwtUserDetailService.java
@@ -1,5 +1,8 @@
 package com.countiespower.api.user.register.service.impl;
 
+import com.countiespower.api.user.register.data.UserEntity;
+import com.countiespower.api.user.register.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,15 +10,25 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class JwtUserDetailService implements UserDetailsService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public JwtUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if ("diego".equals(username)) {
-            return new User("diego","$2y$12$phFqLFpuNJsZ1ixqLqzRFuMUjA2S7RTqQwjfCYVJEcAzCrEDqHh9e", new ArrayList<>());
-        } else {
-            throw new UsernameNotFoundException("User not found!");
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Optional<UserEntity> userEntityOp = userRepository.findByEmailIgnoreCase(email);
+        if (!userEntityOp.isPresent()) {
+            throw new UsernameNotFoundException(email);
         }
+        return new User(userEntityOp.get().getEmail(), userEntityOp.get().getEncryptedPassword(), true, true, true, true, new ArrayList<>());
     }
 }
--- src/main/java/com/countiespower/api/user/register/ui/controller/UsersController.java
+++ src/main/java/com/countiespower/api/user/register/ui/controller/UsersController.java
@@ -1,7 +1,10 @@
 package com.countiespower.api.user.register.ui.controller;
 
+import com.countiespower.api.user.register.config.JwtTokenUtil;
 import com.countiespower.api.user.register.dto.UserDto;
 import com.countiespower.api.user.register.service.UsersService;
+import com.countiespower.api.user.register.ui.model.JwtRequest;
+import com.countiespower.api.user.register.ui.model.JwtResponse;
 import com.countiespower.api.user.register.ui.model.UserResponse;
 import com.countiespower.api.user.register.utils.MapperUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -20,6 +29,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/users")
 public class UsersController {
 
@@ -28,6 +38,15 @@ public class UsersController {
 
     @Autowired
     UsersService usersService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public ResponseEntity<Map<String, Object>> getAllUsers(
@@ -81,6 +100,25 @@ public class UsersController {
     @GetMapping("/status/check")
     public String status() {
         return "Working on port " + env.getProperty("local.server.port");
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
+        authenticate(authenticationRequest.getEmail(), authenticationRequest.getPassword());
+        final UserDetails userDetails = userDetailsService
+                .loadUserByUsername(authenticationRequest.getEmail());
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
     }
 
 }
